fb0bb1fb3c55017354fb6ae0e47491384226b9c0
Arjun Attam <arjunattam@gmail.com>
2018-03-12 14:46:55 +0530

    Setup GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-115538378-1')
+
+exports.onRouteUpdate = (state, page, pages) => {
+  const { pathname } = state
+  ReactGA.set({ page: pathname })
+  ReactGA.pageview(pathname)
+}
