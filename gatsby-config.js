d66ae341ef81536ce78d8f95bc21c7c3b78aa330
Arjun Attam <arjunattam@gmail.com>
2018-03-15 16:59:54 +0530

    Remove google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Rubberduck',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `cabin`,
-          `lato`,
-          `work sans`,
-          `montserrat`,
-          `raleway`,
-          `quicksand`,
-          `rubik`,
-
-          // `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
-        ],
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
